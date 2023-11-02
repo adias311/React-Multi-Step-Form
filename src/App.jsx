c1b7a22d0df69c49ba23dd498783247262524b2c
Adias Afnan Valentino <adiasafnan@gmail.com>
2023-11-02 18:10:33 +0700

    responsive layout in desktop & tablet

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import FormStep from './component/formStep/index';
 import FormPersonal from './component/formPersonal/index';
 import FormPlan from './component/formPlan/index';
 import FormAddOns from './component/formAddOns/index';
 import FormSummary from './component/formSummary/index';
-import FormEnd from './component/formEnd';
+const FormEnd = React.lazy(() => import('./component/formEnd'));
 import { useSelector, useDispatch } from 'react-redux';
 import { getStep } from './redux/actions/formSlice';
-// import {submitForm} from ''
 
-console.log("1");
+
 function App() {
   const dispatch = useDispatch();
   const step = useSelector((state) => state.formSlice.step);
@@ -33,23 +32,26 @@ function App() {
         <FormStep stepNumber={1} title={`YOUR INFO`} isActive={step === 1}></FormStep>
         <FormStep stepNumber={2} title={`SELECT PLAN`} isActive={step === 2}></FormStep>
         <FormStep stepNumber={3} title={`ADD-ONS`} isActive={step === 3}></FormStep>
-        <FormStep stepNumber={4} title={`SUMMARY`} isActive={step === 4 || 5}></FormStep>
+        <FormStep stepNumber={4} title={`SUMMARY`} isActive={step === 4 || step === 5}></FormStep>
       </div>
 
-      <div className="form">
+      <div className="form" id='form_end'>
         <form action="">
           {step === 1 && <FormPersonal />}
           {step === 2 && <FormPlan />}
           {step === 3 && <FormAddOns />}
           {step === 4 && <FormSummary />}
-          {step === 5 && <FormEnd />}
+          <Suspense fallback={<></>}>
+            {step === 5 && <FormEnd />}
+          </Suspense>
 
-          <section className="form_navigate">
-            {step === 1 && <span></span>}
-            {step > 1 && step < 5 && <button className='prev' type="button" onClick={prev}>Go Back</button>}
-            {step < 4 && <button className='next' type="button" onClick={next}>Next Step</button>}
-            {step === 4 && <button className='next' type="button" onClick={() => {next(); submitForm()}}>Confirm</button>}
-          </section>
+          {step !== 5 &&
+            <section className="form_navigate">
+              {step === 1 && <span></span>}
+              {step > 1 && step < 5 && <button className='prev' type="button" onClick={prev}>Go Back</button>}
+              {step < 4 && <button className='next' type="button" onClick={next}>Next Step</button>}
+              {step === 4 && <button className='next' type="button" onClick={() => { next(); submitForm() }}>Confirm</button>}
+            </section>}
         </form>
       </div>
     </div>
--- src/component/formEnd/index.jsx
+++ src/component/formEnd/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import './index.css'
 
 function FormEnd() {
   return (
-    <center>
-      <div>Thank You</div>
-      <img width={250} src="../public/assets/images/icon-thank-you.svg" alt="" />
-    </center>
+    <div className='thank_you'>
+      <img width={100} src="../public/assets/images/icon-thank-you.svg" alt="" />
+      <h1>Thank You!</h1>
+      <p className='thanks' id='thanks'>Thanks for confirming your subscription! We hope you have
+        fun using our platform. If you ever need support, please feel
+        free to email us at support@loremgaming.com.</p>
+    </div>
   )
 }
 
-export default FormEnd
+export default FormEnd;
